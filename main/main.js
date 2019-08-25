fea71d4c2e392e89721c1b7c375e68321d218580
xiongwei <2894001817@qq.com>
2019-08-25 16:18:31 +0800

    通过了测试，添加了概念图，忘记了小步提交

--- main/main.js
+++ main/main.js
@@ -1,9 +1,34 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(firstNum,secondNum) {
+    var bigNum;
+    var smallNum;
+    if(firstNum<secondNum){
+        bigNum = secondNum;
+        smallNum = firstNum;
+    }else{
+        bigNum = firstNum;
+        smallNum = secondNum;
+    }
+    return bigNum%smallNum;
+};
 
-function caclSum() {};
+function caclSum(array) {
+    let result = 0;
+    array.forEach(element => {
+        result += element;
+    });
+    return result;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(array,num) {
+    let result = 0;
+    array.forEach(function(item){
+        if(item<num){
+            result += item;
+        }
+    })
+    return result;
+};
 
 module.exports = {
     caclRemaindar,
